3c12cfac178d064e8ddc7cb7c35aa53844b4722c
J Sohn <son10@ajou.ac.kr>
2020-05-09 01:33:48 +0900

    다이얼로그 추가, 기능 수정

--- my_cert_helper/lib/calendar_page.dart
+++ my_cert_helper/lib/calendar_page.dart
@@ -11,7 +11,7 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 final Map<DateTime, List> _holidays = {
   DateTime(2020, 4, 15): ['21대 국회의원 선거'],
@@ -45,42 +45,57 @@ final dummyttoeic = [
     }
 ];
 
+Map<DateTime, List<dynamic>> cal_events;
 
-class CalenderPage extends StatefulWidget{
-
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
-  State createState() => _CalenderPageState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '캘린더',
+      theme: ThemeData(
+        primarySwatch: Colors.yellow // 주 색상 변경
+      ),
+      home: HomePage(),
+    );
+  }
 }
 
-class _CalenderPageState extends State<CalenderPage>{
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
-  CalendarController _controller;
-  Map<DateTime, List<dynamic>> _events;
-  List<dynamic> _selectedEvents;
-  TextEditingController _eventController;
+class _HomePageState extends State<HomePage> {
+
+  // 여기서 DB 확인 후 캘린더 설정
+
+  
+  CalendarController cal_controller;
+  List<dynamic> cal_selectedEvents;
+  TextEditingController cal_eventController;
   SharedPreferences prefs;
-
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    _controller = CalendarController();
-    _eventController = TextEditingController();
-    _events = {};
-    _selectedEvents = [];
+    cal_controller = CalendarController();
+    cal_eventController = TextEditingController();
+    cal_events = {};
+    cal_selectedEvents = [];
     initPrefs();
-    initializeDateFormatting('pt_BR', null);
   }
+
 
   initPrefs() async {
     prefs = await SharedPreferences.getInstance();
     setState(() {
-      _events = Map<DateTime, List<dynamic>>.from(
+      cal_events = Map<DateTime, List<dynamic>>.from(
           decodeMap(json.decode(prefs.getString("events") ?? "{}")));
     });
   }
-  
+
   Map<String, dynamic> encodeMap(Map<DateTime, dynamic> map) {
     Map<String, dynamic> newMap = {};
     map.forEach((key, value) {
@@ -95,8 +110,8 @@ class _CalenderPageState extends State<CalenderPage>{
       newMap[DateTime.parse(key)] = map[key];
     });
     return newMap;
-  }  
-  
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -109,12 +124,12 @@ class _CalenderPageState extends State<CalenderPage>{
           children: <Widget>[
             TableCalendar(
               locale: 'ko_KO',
-              events: _events,
+              events: cal_events,
               holidays: _holidays,
               //initialCalendarFormat: CalendarFormat.week,
               calendarStyle: CalendarStyle(
-                todayColor: Colors.orange, // 오늘 날짜 동그라미 색상
-                selectedColor: Theme.of(context).primaryColor, // 기본 색상과 동일한 선택날짜 색상
+                todayColor: Colors.green, // 오늘 날짜 동그라미 색상
+                selectedColor: Colors.orange, // 기본 색상과 동일한 선택날짜 색상
                 todayStyle: TextStyle(
                   fontWeight: FontWeight.bold,
                   color: Colors.white,
@@ -124,7 +139,7 @@ class _CalenderPageState extends State<CalenderPage>{
               headerStyle: HeaderStyle(
                 centerHeaderTitle: true,
                 formatButtonDecoration: BoxDecoration(
-                  color: Colors.orange,
+                  color: Colors.green,
                   borderRadius: BorderRadius.circular(20.0),
                 ),
                 formatButtonTextStyle: TextStyle(
@@ -133,22 +148,13 @@ class _CalenderPageState extends State<CalenderPage>{
                 formatButtonShowsNext: false,
               ),
 //              startingDayOfWeek: StartingDayOfWeek.monday,
-              onDaySelected: (date, events){
+              onDaySelected: (date, cal_events){
                 setState(() {
-                  _selectedEvents = events;
+                  cal_selectedEvents = cal_events;
                 });
               },
               builders: CalendarBuilders(
-                selectedDayBuilder: (context, date, events) => 
-                Container(
-                  margin: const EdgeInsets.all(4.0),
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Text(date.day.toString(), style: TextStyle(color:Colors.white),)),
-                todayDayBuilder: (context, date, events) => 
+                selectedDayBuilder: (context, date, cal_events) => 
                 Container(
                   margin: const EdgeInsets.all(4.0),
                   alignment: Alignment.center,
@@ -157,41 +163,118 @@ class _CalenderPageState extends State<CalenderPage>{
                     borderRadius: BorderRadius.circular(10.0),
                   ),
                   child: Text(date.day.toString(), style: TextStyle(color:Colors.white),)),
+                todayDayBuilder: (context, date, cal_events) => 
+                Container(
+                  margin: const EdgeInsets.all(4.0),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Text(date.day.toString(), style: TextStyle(color:Colors.white),)),
               ),
-              calendarController: _controller,
+              calendarController: cal_controller,
               ),
-             ... _selectedEvents.map((event) => ListTile(
-               title: Text(event),
+             ... cal_selectedEvents.map((tevents) => Card(
 
+// or ListTile(title: Text(event),)
+               child: Container(
+                 width: MediaQuery.of(context).size.width,
+                 height: 50,
+                 child: Padding(
+                   padding: EdgeInsets.fromLTRB(20.0, 16.0, 16.0, 16.0),
+                   child: Text(tevents, style: TextStyle(fontSize: 20),)
+                   )
+               ),
              )),
           ]
         )
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        tooltip: '자격증 시험 일정을 추가합니다.',
-        label: Text('일정 추가'),
-        icon: Icon(Icons.add),
-        backgroundColor: Colors.green,
-        foregroundColor: Colors.white,
-        onPressed: () {
-          Navigator.push(context,
-          MaterialPageRoute<void>(builder: (BuildContext context) {
-            return _editTestDay();
-            })
-          );
-        }
+      floatingActionButton: SpeedDial(
+        animatedIcon: AnimatedIcons.menu_close,
+        animatedIconTheme: IconThemeData(size:22.0),
+        closeManually: true,
+        tooltip: '일정을 추가합니다.',
+        child: Icon(Icons.add),
+        onOpen: () => print('일정 추가'),
+        onClose: () => print('Dial Closed'),
+        backgroundColor: Colors.yellow,
+        foregroundColor: Colors.black,
+        curve: Curves.bounceIn,
+        children: [
+          SpeedDialChild(
+            child: Icon(Icons.calendar_today),
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.orange,
+            label: '일정 추가',
+            labelStyle: TextStyle(fontWeight: FontWeight.w500),
+            onTap: () {
+              _addSchedule();
+            },
+          ),
+          SpeedDialChild(
+            child: Icon(Icons.library_books),
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.green,
+            labelStyle: TextStyle(fontWeight: FontWeight.w500),
+            label: '시험 일정 추가',
+            onTap: () {
+              Navigator.push(context,
+              MaterialPageRoute<void>(builder: (BuildContext context) {
+                return editTestDay();
+              })
+             );
+            },
+          ),          
+        ],
       ),
+
+      
+    );
+  }
+
+
+// 일정 추가
+    _addSchedule() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: TextField(
+          controller: cal_eventController,
+        ), //TextField
+        actions: <Widget>[
+          FlatButton(
+            child: Text('저장'),
+            color: Colors.green,
+            onPressed: () {
+              if(cal_eventController.text.isEmpty) return;
+              setState(() {
+                 if(cal_events[cal_controller.selectedDay]!= null) {
+                 cal_events[cal_controller.selectedDay].add(cal_eventController.text);
+                }
+                else {
+                  cal_events[cal_controller.selectedDay] = [cal_eventController.text];
+               }
+               prefs.setString("events", json.encode(encodeMap(cal_events)));
+               cal_eventController.clear();
+               Navigator.pop(context);
+              });
+            },
+          )
+        ],
+      )
     );
   }
 }
 
+// 자격증 시험 일정 추가
 
-class _editTestDay extends StatefulWidget {
+class editTestDay extends StatefulWidget {
   @override
-  __editTestDayState createState() => __editTestDayState();
+  _editTestDayState createState() => _editTestDayState();
 }
 
-class __editTestDayState extends State<_editTestDay> {
+class _editTestDayState extends State<editTestDay> {
 
   @override
   Widget build(BuildContext context) {
@@ -199,18 +282,18 @@ class __editTestDayState extends State<_editTestDay> {
       appBar: AppBar(
         title: Text('자격증 시험 일정 추가')
         ),
-        body: _showTest(context, dummyttoeic),
+        body: showTest(context, dummyttoeic),
     );
   }
 
-  Widget _showTest(BuildContext context, List<Map> snapshot) {
+  Widget showTest(BuildContext context, List<Map> snapshot) {
     return ListView(
       padding: const EdgeInsets.only(top:20.0),
-      children: snapshot.map((data)=> _buildListItem(context,data)).toList()
+      children: snapshot.map((data)=> buildListItem(context,data)).toList()
     );
   }
 
-  Widget _buildListItem(BuildContext context, Map data) {
+  Widget buildListItem(BuildContext context, Map data) {
     final record = Record.fromMap(data);
 
    return Padding(
@@ -224,32 +307,34 @@ class __editTestDayState extends State<_editTestDay> {
        child: ListTile(
          title: Text(record.testname),
          trailing: Text(record.testday.toString()),
-         onTap: () => _showDialog(context,record.testname),
+         onTap: () => cal_showDialog(context,record.testname,record.testday),
        ),
      ),
    );
   }
 
-  void _showDialog(BuildContext context, text) {
-  // 경고창을 보여주는 가장 흔한 방법.
+  void cal_showDialog(BuildContext context, tname, tday) {
+
+    String settname = tname;
+    DateTime settday = DateTime.parse(tday);
+    DateTime _tday = tday;
     showDialog(
       context: context,
         builder: (BuildContext ctx) {
           return AlertDialog(
             title: Text('일정 추가'),
-            content: Text('$text 시험을 캘린더에 추가합니까?.'),
+            content: Text('$settname ($tday) 시험을 캘린더에 추가합니까?'),
             actions: <Widget>[
               FlatButton(
-                child: Text('저장', style: TextStyle(color: Colors.black),),
+                child: Text('저장', style: TextStyle(color: Colors.black, fontSize: 17),),
                 onPressed: () {
-                  
-                },
+                  //Navigator.pop(context);
+                  // DB에 저장
+//                  cal_events.addEntries(Map(tday));
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
+                }
               )
             ],
-            // 주석으로 막아놓은 actions 매개변수도 확인해 볼 것.
-            // actions: <Widget>[
-            //     FlatButton(child: Text('확인'), onPressed: () => Navigator.pop(context)),
-            // ],
         );
       }
     );
